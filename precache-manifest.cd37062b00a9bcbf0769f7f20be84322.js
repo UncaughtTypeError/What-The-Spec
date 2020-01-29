082ca5be6c7b4a5a26138db6478dbf830fac8800
Nathan Shepherd <your@email.goes.here>
2020-01-29 22:45:14 +0200

    initial build subtree commit

---
+++ precache-manifest.cd37062b00a9bcbf0769f7f20be84322.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3568f04cb5315461911bf431c3f6594d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57b5bb4660f5241e22cf",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7a4131963ee8c57925ae",
+    "url": "/static/js/2.e86d75ae.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e86d75ae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "57b5bb4660f5241e22cf",
+    "url": "/static/js/main.497e531a.chunk.js"
+  },
+  {
+    "revision": "a630e406e37adbb9b367",
+    "url": "/static/js/runtime-main.5c812b07.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
