689cc7d9fe3b6ab3afb349b5977768003a71ff54
Nathan Shepherd <your@email.goes.here>
2020-02-01 22:24:04 +0200

    FontAwesome library installation and integration

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/What-The-Spec/precache-manifest.3f88cbed2526867d56cd74a3d0246ccc.js"
+  "/What-The-Spec/precache-manifest.8aeb16d3f958ec2d7441c909e9baa459.js"
 );
 
 self.addEventListener('message', (event) => {
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+// Libraries
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCss3Alt, faHtml5, faJs } from '@fortawesome/free-brands-svg-icons';
+
 const Nav = () => (
     <nav>
         <ul>
             <li>
-                <Link to="/css">CSS</Link>
+                <Link to="/css">
+                    <FontAwesomeIcon icon={faCss3Alt} />
+                    CSS
+                </Link>
             </li>
             <li>
-                <Link to="/html">HTML</Link>
+                <Link to="/html">
+                    <FontAwesomeIcon icon={faHtml5} />
+                    HTML
+                </Link>
             </li>
             <li>
-                <Link to="/javascript">Javascript</Link>
+                <Link to="/javascript">
+                    <FontAwesomeIcon icon={faJs} />
+                    Javascript
+                </Link>
             </li>
         </ul>
     </nav>
