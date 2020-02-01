253425cb0b4406b8fdc28554e8dcc6f996a6ee9b
Nathan Shepherd <your@email.goes.here>
2020-02-02 00:00:04 +0200

    Footer component added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import React from 'react';
 // Components
 import Header from './components/Header';
 import Body from './components/Body';
+import Footer from './components/Footer';
 
 const App = () => (
 
   <Router>
     <Header/>
     <Body/>
+    <Footer/>
   </Router>
-  
+
 );
 
 export default App;
