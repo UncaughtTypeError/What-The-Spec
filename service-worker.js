4ab9d21ab644149abb314a9bf7f2b666919de16c
Nathan Shepherd <your@email.goes.here>
2020-02-02 00:03:02 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/What-The-Spec/precache-manifest.e5c579596edb0acb548391a6d248d2c4.js"
+  "/What-The-Spec/precache-manifest.9c600dc11ca4e1fd7227b893308684a3.js"
 );
 
 self.addEventListener('message', (event) => {
