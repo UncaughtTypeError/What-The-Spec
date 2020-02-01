108eb9671632ca2908c4a7fa6c1e3eb59223351d
Nathan Shepherd <your@email.goes.here>
2020-02-01 22:14:45 +0200

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import React, { Suspense, lazy } from 'react';
 
+// Components
 import RouterErrorBoundry from './errorboundries/RouterErrorBoundry';
 import Nav from './components/Nav';
 
