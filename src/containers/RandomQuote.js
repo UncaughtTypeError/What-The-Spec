001904e635d5dcba69e1310257cd3f0292782382
Nathan Shepherd <your@email.goes.here>
2020-02-02 12:46:52 +0200

    refactoring

--- src/containers/RandomQuote.js
+++ src/containers/RandomQuote.js
@@ -1,4 +1,6 @@
 import React, { useEffect } from 'react';
+
+// Redux
 import { useDispatch, useSelector } from 'react-redux';
 
 // Actions
@@ -6,20 +8,19 @@ import { updateQuote } from '../redux/actions';
 
 function RandomQuote() {
 
-const   quote = useSelector(state => state.updateQuote.quote),
-        cite = useSelector(state => state.updateQuote.cite),
-        dispatch = useDispatch(),
-        props = { quote, cite, dispatch };
+    const   quote = useSelector(state => state.updateQuote.quote),
+            cite = useSelector(state => state.updateQuote.cite),
+            dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(updateQuote());
     }, [dispatch]);
     
     return (
-        props.quote ?
+        quote ?
             <blockquote>
-                {props.quote}
-                <cite> ~ {props.cite}</cite>
+                {quote}
+                <cite> ~ {cite}</cite>
             </blockquote>
         : 
             <blockquote>
