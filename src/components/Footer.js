075374bbdbb571bea8d125e400bd365192ff0481
Nathan Shepherd <your@email.goes.here>
2020-02-02 18:27:56 +0200

    links and credits added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,11 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 const Footer = () => (
 
     <footer>
-        <small>
-            --- WIP ---<br/>
-            <FontAwesomeIcon icon={faGithub} /> <a href="https://github.com/UncaughtTypeError/What-The-Spec/" target="_blank" rel="noopener" title="Github Repo"><em>What The Spec?</em></a>
-        </small>
+        <ul>
+            <li><em>What The Spec?</em></li>
+            <li>Source code on <FontAwesomeIcon icon={faGithub} /> <a href="https://github.com/UncaughtTypeError/What-The-Spec/" target="_blank" rel="noopener" title="Github Repo">GitHub</a></li> 
+            <li>Hosted on, and deployed by <a href="https://netlify.com/" target="_blank" rel="noopener" title="Netlify">Netlify <img width="80" loading="lazy" alt="Netlify" src="https://www.netlify.com/img/global/badges/netlify-dark.svg"/></a></li>
+        </ul>
     </footer>
 
 );
