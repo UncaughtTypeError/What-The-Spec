2e1d9e202db8238f8ca3c3a9ea8e3b4c0d025ab0
Nathan Shepherd <your@email.goes.here>
2020-02-02 13:12:00 +0200

    action import, container store connection, props and dispatch mapping with hooks

--- src/containers/Nav.js
+++ src/containers/Nav.js
@@ -1,35 +1,50 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+// Redux
+import { useDispatch, useSelector } from 'react-redux';
+
+// Actions
+import { updateView } from '../redux/actions';
+
 // Libraries
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCss3Alt, faHtml5, faJs } from '@fortawesome/free-brands-svg-icons';
 
-const Nav = () => (
+function Nav() {
 
-    <nav>
-        <ul>
-            <li className='css'>
-                <Link to='/css'>
-                    <FontAwesomeIcon icon={faCss3Alt} />
-                    CSS
-                </Link>
-            </li>
-            <li className='html'>
-                <Link to='/html'>
-                    <FontAwesomeIcon icon={faHtml5} />
-                    HTML
-                </Link>
-            </li>
-            <li className='js'>
-                <Link to='/javascript'>
-                    <FontAwesomeIcon icon={faJs} />
-                    Javascript
-                </Link>
-            </li>
-        </ul>
-    </nav>
+    const   view = useSelector(state => state.updateSpecView.view),
+            dispatch = useDispatch();
+
+    const setView = (view) => {
+        dispatch(updateView(view));
+    }
+
+    return (
+        <nav data-view={view}>
+            <ul>
+                <li className='css'>
+                    <Link to='/css' onClick={() => setView('css')}>
+                        <FontAwesomeIcon icon={faCss3Alt} />
+                        CSS
+                    </Link>
+                </li>
+                <li className='html'>
+                    <Link to='/html' onClick={() => setView('html')}>
+                        <FontAwesomeIcon icon={faHtml5} />
+                        HTML
+                    </Link>
+                </li>
+                <li className='js' onClick={() => setView('js')}>
+                    <Link to='/javascript'>
+                        <FontAwesomeIcon icon={faJs} />
+                        Javascript
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
     
-);
+};
 
 export default Nav;
