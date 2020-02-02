b179d72824777e190120bd0fb1689622f1e12326
Nathan Shepherd <your@email.goes.here>
2020-02-02 11:03:23 +0200

    store creation, reducer combination, Provider setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Redux
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import { updateQuote, updateSpecView } from './redux/reducers';
+
+// App
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Store
+const   rootReducer = combineReducers({ updateQuote, updateSpecView }),
+        store = createStore(rootReducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
