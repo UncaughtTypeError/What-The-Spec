1ea2e64840d75068720a51648057d7c0521943ce
Nathan Shepherd <your@email.goes.here>
2020-02-02 18:07:31 +0200

    spec data objects defined

---
+++ src/utils/spec-data.js
@@ -0,0 +1,110 @@
+export const CSS_spec = [
+    {
+        acronym: 'FPWD',
+        stage: 'First Public Working Draft',
+        phase: 'Draft (introductory phase)',
+        definition: 'A First Public Working Draft is the first official document that W3C has published for initial review by the community, including W3C Members, the public, and other technical organizations.',
+        organisation: 'W3C'
+    },
+    {
+        acronym: 'WD',
+        stage: 'Working Draft',
+        phase: 'Draft (design phase)',
+        definition: 'A Working Draft is a document that W3C has published for review by the community, including W3C Members, the public, and other technical organizations.',
+        organisation: 'W3C'
+    },
+    {
+        acronym: 'CR',
+        stage: 'Candidate Recommendation',
+        phase: 'Review (testing & implementation phase)',
+        definition: 'A Candidate Recommendation is a document that W3C believes has been widely reviewed and satisfies the Working Group\'s technical requirements. W3C publishes a Candidate Recommendation to gather implementation experience.',
+        organisation: 'W3C'
+    },
+    {
+        acronym: 'PR',
+        stage: 'Proposed Recommendation',
+        phase: 'Review (approval phase)',
+        definition: 'A Proposed Recommendation is a mature technical report that, after wide review for technical soundness and implementability, W3C has sent to the W3C Advisory Committee for final endorsement.',
+        organisation: 'W3C'
+    },
+    {
+        acronym: 'REC',
+        stage: 'Recommendation',
+        phase: 'Endorsed (maintenance phase)',
+        definition: 'A W3C Recommendation is a specification or set of guidelines that, after extensive consensus-building, has received the endorsement of W3C Members and the Director. W3C recommends the wide deployment of its Recommendations.',
+        organisation: 'W3C'
+    }
+];
+
+export const HTML_spec = [
+    {
+        acronym: 'FPWD',
+        stage: 'First Public Working Draft',
+        phase: 'Draft (introductory phase)',
+        definition: 'A First Public Working Draft is the first official document that W3C has published for initial review by the community, including W3C Members, the public, and other technical organizations.',
+        organisation: 'W3C'
+    },
+    {
+        acronym: 'WD',
+        stage: 'Working Draft',
+        phase: 'Draft (design phase)',
+        definition: 'A Working Draft is a document that W3C has published for review by the community, including W3C Members, the public, and other technical organizations.',
+        organisation: 'W3C'
+    },
+    {
+        acronym: 'CR',
+        stage: 'Candidate Recommendation',
+        phase: 'Review (testing & implementation phase)',
+        definition: 'A Candidate Recommendation is a document that W3C believes has been widely reviewed and satisfies the Working Group\'s technical requirements. W3C publishes a Candidate Recommendation to gather implementation experience.',
+        organisation: 'W3C'
+    },
+    {
+        acronym: 'PR',
+        stage: 'Proposed Recommendation',
+        phase: 'Review (approval phase)',
+        definition: 'A Proposed Recommendation is a mature technical report that, after wide review for technical soundness and implementability, W3C has sent to the W3C Advisory Committee for final endorsement.',
+        organisation: 'W3C'
+    },
+    {
+        acronym: 'REC',
+        stage: 'Recommendation',
+        phase: 'Endorsed (maintenance phase)',
+        definition: 'A W3C Recommendation is a specification or set of guidelines that, after extensive consensus-building, has received the endorsement of W3C Members and the Director. W3C recommends the wide deployment of its Recommendations.',
+        organisation: 'W3C'
+    },
+    {
+        acronym: 'LS',
+        stage: 'Living Standard',
+        phase: 'Evergreen (continuous maintenance)',
+        definition: 'An HTML specification that is a living document, standards that are continuously updated and maintained as necessary.',
+        organisation: 'WHATWG'
+    }
+];
+
+export const JS_spec = [
+    {
+        stage: 'Strawperson',
+        definition: 'Allow input into the specification.',
+        organisation: 'TC39'
+    },
+    {
+        stage: 'Proposal',
+        definition: 'Make the case for the addition, describe the shape of a solution, identify potential challenges',
+        organisation: 'TC39'
+    },
+    {
+        stage: 'Draft',
+        definition: 'Precisely describe the syntax and semantics using formal spec language',
+        organisation: 'TC39'
+    },
+    {
+        stage: 'Candidate',
+        definition: 'Indicate that further refinement will require feedback from implementations and users',
+        organisation: 'TC39'
+    },
+    {
+        stage: 'Finished',
+        definition: 'Indicate that the addition is ready for inclusion in the formal ECMAScript standard',
+        organisation: 'TC39'
+    }
+];
