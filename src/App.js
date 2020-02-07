b125fad95d25a8c1704fd6f650a36e7ac516259a
Nathan Shepherd <your@email.goes.here>
2020-02-07 22:08:37 +0200

    Route path updates

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Footer from './components/Footer';
 
 const App = () => (
 
-  <Router basename='/'>
+  <Router basename={`${process.env.PUBLIC_URL}/`}>
     <Header/>
     <Body/>
     <Footer/>
