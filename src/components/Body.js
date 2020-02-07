f23170fd6a27dab0393a63a573b114ecf4502f02
Nathan Shepherd <your@email.goes.here>
2020-02-07 21:27:00 +0200

    hash based routing for UI syncing with url

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 
 // Styles
 import styles from '../styles/Body.module.css';
@@ -10,19 +10,23 @@ import RouterErrorBoundry from '../errorboundries/RouterErrorBoundry';
 const   Home = lazy(() => import('../routes/Home')),
         CSS = lazy(() => import('../routes/CSS')),
         HTML = lazy(() => import('../routes/HTML')),
-        Javascript = lazy(() => import('../routes/Javascript'));
+        Javascript = lazy(() => import('../routes/Javascript')),
+        NoMatch = lazy(() => import('../routes/NoMatch'));
 
 const Body = () => (
 
     <RouterErrorBoundry>
         <section className={styles.base}>
             <Suspense fallback={<div className={styles.title}>Loading...</div>}>
-                <Switch>
-                    <Route exact path='/' component={Home}/>
-                    <Route path={'/css'} component={CSS}/>
-                    <Route path={'/html'} component={HTML}/>
-                    <Route path={'/javascript'} component={Javascript}/>
-                </Switch>
+                <HashRouter basename='/'>
+                    <Switch>
+                        <Route exact path='/' component={Home}/>
+                        <Route path={'/css'} component={CSS}/>
+                        <Route path={'/html'} component={HTML}/>
+                        <Route path={'/javascript'} component={Javascript}/>
+                        <Route component={NoMatch}/>
+                    </Switch>
+                </HashRouter>
             </Suspense>
         </section>
     </RouterErrorBoundry>
