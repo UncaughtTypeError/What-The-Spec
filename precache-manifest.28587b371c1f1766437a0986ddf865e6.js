62d11cac9c483567dd4c4925acd2b8e7da4028ff
Nathan Shepherd <your@email.goes.here>
2020-02-07 22:49:13 +0200

    Updates

---
+++ precache-manifest.28587b371c1f1766437a0986ddf865e6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1cc70a1723b68ecfc950c30d5bacb88",
+    "url": "/What-The-Spec/index.html"
+  },
+  {
+    "revision": "3fe113061a77bd3979b6",
+    "url": "/What-The-Spec/static/css/3.cec13087.chunk.css"
+  },
+  {
+    "revision": "23d4013ee53e86218c5b",
+    "url": "/What-The-Spec/static/css/4.cec13087.chunk.css"
+  },
+  {
+    "revision": "8cd66857227bba168bf0",
+    "url": "/What-The-Spec/static/css/5.cec13087.chunk.css"
+  },
+  {
+    "revision": "ad6cdc49af10f154865c",
+    "url": "/What-The-Spec/static/css/6.e780814d.chunk.css"
+  },
+  {
+    "revision": "1cec677e5ace2f4a0372",
+    "url": "/What-The-Spec/static/css/main.3b49d78a.chunk.css"
+  },
+  {
+    "revision": "ab7e6a6ec692406a2af6",
+    "url": "/What-The-Spec/static/js/2.63e76d69.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/What-The-Spec/static/js/2.63e76d69.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3fe113061a77bd3979b6",
+    "url": "/What-The-Spec/static/js/3.2d15ad93.chunk.js"
+  },
+  {
+    "revision": "23d4013ee53e86218c5b",
+    "url": "/What-The-Spec/static/js/4.8748ee62.chunk.js"
+  },
+  {
+    "revision": "8cd66857227bba168bf0",
+    "url": "/What-The-Spec/static/js/5.0bbde2bd.chunk.js"
+  },
+  {
+    "revision": "ad6cdc49af10f154865c",
+    "url": "/What-The-Spec/static/js/6.7335ec48.chunk.js"
+  },
+  {
+    "revision": "eec4b893f913997dcc9c",
+    "url": "/What-The-Spec/static/js/7.fff34691.chunk.js"
+  },
+  {
+    "revision": "1cec677e5ace2f4a0372",
+    "url": "/What-The-Spec/static/js/main.be48f7a8.chunk.js"
+  },
+  {
+    "revision": "f3752e79d16f0124d96b",
+    "url": "/What-The-Spec/static/js/runtime-main.f1d0f519.js"
+  }
+]);
