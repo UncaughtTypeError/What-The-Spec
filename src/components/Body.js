6674fe7b37b1e900c16a88f1f8c771ea9829ea11
Nathan Shepherd <your@email.goes.here>
2020-02-07 21:43:55 +0200

    Router component removed

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy } from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 // Styles
 import styles from '../styles/Body.module.css';
@@ -18,15 +18,13 @@ const Body = () => (
     <RouterErrorBoundry>
         <section className={styles.base}>
             <Suspense fallback={<div className={styles.title}>Loading...</div>}>
-                <HashRouter basename='/'>
-                    <Switch>
-                        <Route exact path='/' component={Home}/>
-                        <Route path={'/css'} component={CSS}/>
-                        <Route path={'/html'} component={HTML}/>
-                        <Route path={'/javascript'} component={Javascript}/>
-                        <Route component={NoMatch}/>
-                    </Switch>
-                </HashRouter>
+                <Switch>
+                    <Route exact path='/' component={Home}/>
+                    <Route path={'/css'} component={CSS}/>
+                    <Route path={'/html'} component={HTML}/>
+                    <Route path={'/javascript'} component={Javascript}/>
+                    <Route component={NoMatch}/>
+                </Switch>
             </Suspense>
         </section>
     </RouterErrorBoundry>
