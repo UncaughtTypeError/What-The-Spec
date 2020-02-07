cf2f87e524c36b3e4d936de3668128c94b2d71da
Nathan Shepherd <your@email.goes.here>
2020-02-07 21:53:52 +0200

    Updates

---
+++ precache-manifest.2f174596c119ba64931c9fac198d5531.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82283dcf39551f0ea08ba5fb370eb361",
+    "url": "/What-The-Spec/index.html"
+  },
+  {
+    "revision": "3fe113061a77bd3979b6",
+    "url": "/What-The-Spec/static/css/3.cec13087.chunk.css"
+  },
+  {
+    "revision": "23d4013ee53e86218c5b",
+    "url": "/What-The-Spec/static/css/4.cec13087.chunk.css"
+  },
+  {
+    "revision": "8cd66857227bba168bf0",
+    "url": "/What-The-Spec/static/css/5.cec13087.chunk.css"
+  },
+  {
+    "revision": "ad6cdc49af10f154865c",
+    "url": "/What-The-Spec/static/css/6.e780814d.chunk.css"
+  },
+  {
+    "revision": "5057bcbfdc1211ff0455",
+    "url": "/What-The-Spec/static/css/main.3b49d78a.chunk.css"
+  },
+  {
+    "revision": "ab7e6a6ec692406a2af6",
+    "url": "/What-The-Spec/static/js/2.63e76d69.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/What-The-Spec/static/js/2.63e76d69.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3fe113061a77bd3979b6",
+    "url": "/What-The-Spec/static/js/3.2d15ad93.chunk.js"
+  },
+  {
+    "revision": "23d4013ee53e86218c5b",
+    "url": "/What-The-Spec/static/js/4.8748ee62.chunk.js"
+  },
+  {
+    "revision": "8cd66857227bba168bf0",
+    "url": "/What-The-Spec/static/js/5.0bbde2bd.chunk.js"
+  },
+  {
+    "revision": "ad6cdc49af10f154865c",
+    "url": "/What-The-Spec/static/js/6.7335ec48.chunk.js"
+  },
+  {
+    "revision": "eec4b893f913997dcc9c",
+    "url": "/What-The-Spec/static/js/7.fff34691.chunk.js"
+  },
+  {
+    "revision": "5057bcbfdc1211ff0455",
+    "url": "/What-The-Spec/static/js/main.103bc179.chunk.js"
+  },
+  {
+    "revision": "f3752e79d16f0124d96b",
+    "url": "/What-The-Spec/static/js/runtime-main.f1d0f519.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2dee2b19d5175f5f13a319483e33ce29.js"
+  "/What-The-Spec/precache-manifest.2f174596c119ba64931c9fac198d5531.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/What-The-Spec/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
