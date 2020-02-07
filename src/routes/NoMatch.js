e5c9b66e30bc0dbff6b8e91f25a2d171c4691c4f
Nathan Shepherd <your@email.goes.here>
2020-02-07 21:12:33 +0200

    catch-all mmatch route added

---
+++ src/routes/NoMatch.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NoMatch = () => (
+    
+    <div style={{fontWeight: 'normal'}}>
+        <h1>Oops! We've got nothing.</h1>
+        <h3>Error loading route</h3>
+    </div>
+
+);
+
+export default NoMatch;
