82de492ddc386a041cfb80f7ed2ccc8f6999c258
Nathan Shepherd <your@email.goes.here>
2020-02-09 15:02:24 +0200

    formatting

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,11 @@ const Header = () => (
 
     <header className={styles.base}>
         <hgroup>
-            <h1><span>W</span>hat <span>T</span>he <span>S</span>pec?</h1>
+            <h1>
+                <span>
+                    <span>W</span>hat <span>T</span>he <span>S</span>pec<span>?</span>
+                </span>
+            </h1>
             <h2>Definitions on proposal stages for CSS, HTML & Javascript specifications</h2>
         </hgroup>
         <Nav/>
